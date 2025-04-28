62c676dde3c68d369badabf160e7d88f1ce2b6f1
Boquan Fang <bqfang@amazon.com>
2025-04-28 16:59:33 +0000

    in progress investigation

---
+++ examples/echo/src/bin/quic_echo_lots_of_clients.rs
@@ -0,0 +1,34 @@
+use futures::future::join_all;
+use s2n_quic::{client::Connect, Client};
+use std::{error::Error, net::SocketAddr};
+
+/// NOTE: this certificate is to be used for demonstration purposes only!
+pub static CERT_PEM: &str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/../../quic/s2n-quic-core/certs/cert.pem"
+));
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    loop {
+        let mut fut = vec![];
+        for _ in 0..100 {
+            fut.push(tokio::spawn(run()));
+        }
+        join_all(fut).await;
+    }
+    Ok(())
+}
+
+async fn run() {
+    let client = Client::builder()
+        .with_tls(CERT_PEM)
+        .unwrap()
+        .with_io("0.0.0.0:0")
+        .unwrap()
+        .start()
+        .unwrap();
+    let addr: SocketAddr = "127.0.0.1:4433".parse().unwrap();
+    let connect = Connect::new(addr).with_server_name("localhost");
+    let mut connection = client.connect(connect).await.unwrap();
+}
