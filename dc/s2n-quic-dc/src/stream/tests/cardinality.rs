944fa8a5f7e95c897b8f1ab221cc97407b7d3bdd
Boquan <boquanfang3@gmail.com>
2025-08-22 17:15:18 +0000

    finish cardinality tests

--- dc/s2n-quic-dc/src/stream/tests/cardinality.rs
+++ dc/s2n-quic-dc/src/stream/tests/cardinality.rs
@@ -8,7 +8,6 @@ use crate::{
 };
 use s2n_quic_core::time::StdClock;
 use std::{collections::HashSet, net::SocketAddr, num::NonZeroUsize, time::Duration};
-use tracing::info;
 
 #[tokio::test]
 async fn many_servers() {
@@ -108,9 +107,111 @@ async fn many_servers() {
             let x = tokio::time::timeout(Duration::from_secs(5), server.accept())
                 .await
                 .unwrap();
-            tracing::info!("accept: {:?}", x);
             x
         })
+        .unwrap();
+    }
+}
+
+#[tokio::test]
+async fn many_clients() {
+    init_tracing();
+
+    let tls_materials_provider = TestTlsProvider {};
+    let test_event_subscriber = NoopSubscriber {};
+    let target = 5000;
+
+    let hs = ServerProvider::builder()
+        .start(
+            "127.0.0.1:0".parse().unwrap(),
+            tls_materials_provider.clone(),
+            test_event_subscriber.clone(),
+            Map::new(
+                Signer::new(b"default"),
+                target * 5,
+                StdClock::default(),
+                test_event_subscriber.clone(),
+            ),
+        )
+        .await
+        .unwrap();
+
+    let server = ServerTokio::<ServerProvider, NoopSubscriber>::builder()
+        .with_address("127.0.0.1:0".parse().unwrap())
+        .with_protocol(Protocol::Tcp)
+        .with_udp(false)
+        .with_workers(NonZeroUsize::new(1).unwrap())
+        .build(hs, test_event_subscriber.clone())
+        .unwrap();
+
+    let mut clients: Vec<Map> = vec![];
+    for _id in 0..target {
+        let map = Map::new(
+            Signer::new(b"default"),
+            1,
+            StdClock::default(),
+            test_event_subscriber.clone(),
+        );
+        let client = ClientProvider::builder()
+            .start(
+                "127.0.0.1:0".parse().unwrap(),
+                map.clone(),
+                tls_materials_provider.clone(),
+                test_event_subscriber.clone(),
+                query_event,
+                SNI.to_string(),
+            )
+            .unwrap();
+        client
+            .handshake_with(
+                server.handshake_addr().unwrap(),
+                query_event,
+                SNI.to_string(),
+            )
+            .await
+            .unwrap();
+        clients.push(map);
+    }
+
+    // Now confirm we can connect **without** handshaking.
+    for client in clients.iter() {
+        let client = ClientProvider::builder()
+            .start(
+                "127.0.0.1:0".parse().unwrap(),
+                client.clone(),
+                tls_materials_provider.clone(),
+                test_event_subscriber.clone(),
+                query_event,
+                SNI.to_string(),
+            )
+            .unwrap();
+        let client = ClientTokio::<ClientProvider, NoopSubscriber>::builder()
+            .with_tcp(true)
+            .with_default_protocol(Protocol::Tcp)
+            .build(client, test_event_subscriber.clone())
+            .unwrap();
+        let client_conn = tokio::net::TcpStream::connect(server.acceptor_addr().unwrap())
+            .await
+            .unwrap();
+
+        // if both are OK we successfully wrote the prelude and it was accepted, so no handshake
+        // was needed.
+        tokio::try_join!(
+            async {
+                let x = client
+                    .connect_tcp_with(server.handshake_addr().unwrap(), client_conn)
+                    .await;
+                x
+            },
+            async {
+                // if we hit an error accepting the stream, this would otherwise just hang
+                // indefinitely -- errors aren't propagated up to accept().
+                let x = tokio::time::timeout(Duration::from_secs(5), server.accept())
+                    .await
+                    .unwrap();
+                x
+            }
+        )
         .unwrap();
     }
 }
