cc03f96e1e114352729872e1f4c3ba29384f4435
Boquan <boquanfang3@gmail.com>
2025-08-22 00:41:41 +0000

    add many servers tests

--- dc/s2n-quic-dc/src/stream/tests.rs
+++ dc/s2n-quic-dc/src/stream/tests.rs
@@ -3,6 +3,9 @@
 
 mod accept_queue;
 mod behavior;
+/// A set of tests ensuring we support a large number of peers.
+#[cfg(future)] // TODO remove this since they're quite expensive
+mod cardinality;
 mod deterministic;
 mod idle_timeout;
 mod key_update;
