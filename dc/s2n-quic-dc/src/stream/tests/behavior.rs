81d3830091618302994e4ac67f779c3e71e7a763
Boquan <boquanfang3@gmail.com>
2025-08-22 17:27:38 +0000

    implement behavior testings in progress

--- dc/s2n-quic-dc/src/stream/tests/behavior.rs
+++ dc/s2n-quic-dc/src/stream/tests/behavior.rs
@@ -1,1 +1,36 @@
 #![allow(clippy::duplicate_mod)] // we're loading the same modules twice to test the API behavior
+
+use crate::stream::testing;
+
+mod differential;
+
+#[path = "behavior"]
+mod tcp {
+    use super::testing::tcp::*;
+
+    mod api;
+    mod handshake;
+    mod read;
+    mod write;
+}
+
+#[path = "behavior"]
+mod dcquic_tcp {
+    use super::testing::dcquic::tcp::*;
+
+    mod api;
+    mod handshake;
+    mod read;
+    mod write;
+}
+
+#[path = "behavior"]
+#[cfg(not(target_os = "macos"))] // TODO fix macos
+mod dcquic_udp {
+    use super::testing::dcquic::udp::*;
+
+    mod api;
+    mod handshake;
+    mod read;
+    mod write;
+}
