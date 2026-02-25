d80e2bd34c81a53046f1c293223d86ac0c5af721
boquan-fang <boquanfang3@gmail.com>
2026-02-25 21:40:33 +0000

    refactor(s2n-quic): use AwsLc as the entropy source

--- dc/s2n-quic-dc/src/random.rs
+++ dc/s2n-quic-dc/src/random.rs
@@ -1,42 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use rand_chacha::rand_core::TryRng;
-
 pub use s2n_quic_core::random::*;
 
-struct AwsLc;
+use s2n_quic::provider::random::{AwsLc, Random as Inner};
 
-impl TryRng for AwsLc {
-    type Error = core::convert::Infallible;
-
-    #[inline]
-    fn try_next_u32(&mut self) -> Result<u32, Self::Error> {
-        let mut v = [0; 4];
-        aws_lc_rs::rand::fill(&mut v).unwrap();
-        Ok(u32::from_ne_bytes(v))
-    }
-
-    #[inline]
-    fn try_next_u64(&mut self) -> Result<u64, Self::Error> {
-        let mut v = [0; 8];
-        aws_lc_rs::rand::fill(&mut v).unwrap();
-        Ok(u64::from_ne_bytes(v))
-    }
-
-    #[inline]
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Self::Error> {
-        aws_lc_rs::rand::fill(dest).unwrap();
-        Ok(())
-    }
-}
-
-pub struct Random(s2n_quic::provider::random::Random<AwsLc>);
+pub struct Random(Inner<AwsLc>);
 
 impl Default for Random {
     #[inline]
     fn default() -> Self {
-        Self(s2n_quic::provider::random::Random::new(AwsLc, AwsLc))
+        Self(Inner::new(AwsLc, AwsLc))
     }
 }
 
